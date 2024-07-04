3b4ad0c04d737073130b62aad831f67d4d055382
YEIN <145302541+yeinP@users.noreply.github.com>
2024-07-04 09:28:04 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,35 @@ const todoForm = document.querySelector("#todo-form");
 const todoList = document.getElementById("todo-list");
 const todoInput = todoForm.querySelector("input");
 
+let todo =[]
+
+function saveTodo(){
+    localStorage.setItem("todo" , JSON.stringify(todo));
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
 function painTodo(newTodo){
-    console.log("i will paint")
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    span.innerText(newTodo);
+    button.innerText="x";
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+    button.addEventListener("click", deleteTodo);
 }
 function handleTodoSubmit(event){
     event.preventDefault();
     const newTodo = todoInput.value;
-    console.log(newTodo);
+    
     todoInput.value = "";
-    console.log(newTodo, todoInput.value, "todoValue")
+    todo.push(newTodo);
+    paintTodo(newTodo);
+    saveTodo();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
